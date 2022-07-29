3129670d68155f44d515a5a571f7a4b82163e9fc
Sebastian Rodriguez <desarrollador2@azsmart.net>
2022-07-29 11:51:27 -0500

    premios_platzi

--- premiosplatzi/asgi.py
+++ premiosplatzi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for premiosPlatziapp project.
+ASGI config for premiosplatzi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'premiosPlatziapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'premiosplatzi.settings')
 
 application = get_asgi_application()
--- premiosplatzi/urls.py
+++ premiosplatzi/urls.py
@@ -1,4 +1,4 @@
-"""premiosPlatziapp URL Configuration
+"""premiosplatzi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('polls.urls')),
+    path('polls/', include('polls.urls')),
 ]
